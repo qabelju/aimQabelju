e4e862bfffe1aaaf4f7b90bde3e73f0dbc9398f4
mehrdad qabelju <qabelju@live.com>
2023-05-03 15:57:02 +0430

    14020213 office 01

---
+++ aim/app/src/main/java/com/example/aim/ItemList.java
@@ -0,0 +1,12 @@
+package com.example.aim;
+
+public class ItemList
+{
+    public int Value ;
+    public String Text;
+
+    @Override
+    public  String toString(){
+        return  Text;
+    }
+}
--- aim/app/src/main/java/com/example/aim/MainActivity.java
+++ aim/app/src/main/java/com/example/aim/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +18,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+
+
+
+
     }
 
+public void Enginee()
+{
+    List<ItemList> bgList=new ArrayList<ItemList>();
 
+    ItemList bg=new ItemList();
+    bg.Value=1;
+    bg.Text="هزینه اول";
+
+    bgList.add(bg);
+
+    ItemList bg2=new  ItemList();
+    bg2.Value=2;
+    bg2.Text="هزینه دوم";
+
+    bgList.add(bg2);
+
+    Spinner spinner=(Spinner)findViewById(R.id.spinnerYear);
+
+    ArrayAdapter<ItemList> dataAdapter=
+            new ArrayAdapter<ItemList> (this,android.R.layout.simple_spinner_dropdown_item,bgList);
+    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(dataAdapter);
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+        @Override
+        public void onItemSelected(AdapterView<?>adapterView,View view,int i,long l){
+            Toast.makeText(MainActivity.this,spinner.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?>adapterView){
+            Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+        }
+    });
+}
 
     public void onclick_EnterPrice(View view)
     {
         setContentView(R.layout.enter_price);
+        Enginee();
     }
 
     public void onclick_ReportList(View view)
@@ -29,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
     {
         setContentView(R.layout.enter_price);
     }
+
+
 
 
 
